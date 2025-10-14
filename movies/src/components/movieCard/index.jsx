4d2148923543e453a0936d157fb3d1cebea47075
Your Name <you@example.com>
2025-10-14 05:40:15 +0100

    Card icons - make actions on movie card configurable.

--- movies/src/components/movieCard/index.jsx
+++ movies/src/components/movieCard/index.jsx
@@ -17,7 +17,7 @@ import img from '../../images/film-poster-placeholder.png'
 import { Link } from "react-router";
 
 
-export default function MovieCard({movie}) {
+export default function MovieCard({movie, action}) {
     const { favorites, addToFavorites } = useContext(MoviesContext);
 
   if (favorites.find((id) => id === movie.id)) {
@@ -71,16 +71,18 @@ export default function MovieCard({movie}) {
           </Grid>
         </Grid>
       </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites" onClick={handleAddToFavorite}>
-        <FavoriteIcon color="primary" fontSize="large" />
-        </IconButton>
+            <CardActions disableSpacing>
+      
+        {action(movie)}
+      
         <Link to={`/movies/${movie.id}`}>
-        <Button variant="outlined" size="medium" color="primary">
-          More Info ...
-        </Button>
+          <Button variant="outlined" size="medium" color="primary">
+            More Info ...
+          </Button>
         </Link>
+        
       </CardActions>
+
     </Card>
   );
 }
--- movies/src/pages/homePage.jsx
+++ movies/src/pages/homePage.jsx
@@ -3,6 +3,7 @@ import { getMovies } from "../api/tmdb-api";
 import PageTemplate from '../components/templateMovieListPage';
 import { useQuery } from '@tanstack/react-query';
 import Spinner from '../components/spinner';
+import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
 
 const HomePage = (props) => {
 
@@ -30,7 +31,9 @@ const HomePage = (props) => {
     <PageTemplate
       title='Discover Movies'
       movies={movies}
-      selectFavorite={addToFavorites}
+      action={(movie) => {
+          return <AddToFavoritesIcon movie={movie} />
+        }}
     />
   );
 };
